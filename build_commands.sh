32d09ee561ad773ac1432af251391284f18628dd
SenX <princekarlagas@gmail.com>
2025-07-03 07:59:54 +0800

    init lunaris

--- build_commands.sh
+++ build_commands.sh
@@ -1,56 +1,42 @@
 #! /bin/bash
 
-rm -rf .repo/local_manifests; \
+rm -rf .repo/local_manifests
+#repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
 
-#repo init -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs; \
-#repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs; \
-#repo init -u https://github.com/ProjectInfinity-X/manifest -b 15 --git-lfs; \
-#repo init -u https://github.com/ProjectMatrixx/android -b 15.0 --git-lfs; \
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs; \
+rm -rf prebuilts/clang/host/linux-x86
 
-rm -rf prebuilts/clang/host/linux-x86; \
+git clone https://github.com/SenseiiX/local_manifest --depth 1 -b lunaris .repo/local_manifests
 
-/opt/crave/resync.sh; \
+/opt/crave/resync.sh 
 
-rm -rf out/target/product/munch; \
-rm -rf device/xiaomi/munch; \
-rm -rf kernel/xiaomi/munch; \
-rm -rf vendor/xiaomi/munch; \
-rm -rf vendor/xiaomi/munch-firmware; \
-rm -rf hardware/xiaomi; \
-rm -rf hardware/dolby; \
-rm -rf vendor/xiaomi/miuicamera; \
-rm -rf packages/resources/devicesettings; \
+# ======CLEAN BUILD=======
+rm -rf out/target/product/munch
+#rm -rf device/xiaomi/munch
+#rm -rf kernel/xiaomi/munch
+#rm -rf vendor/xiaomi/munch
+#rm -rf vendor/xiaomi/munch-firmware
+#rm -rf hardware/xiaomi
+#rm -rf hardware/dolby
+#rm -rf vendor/xiaomi/miuicamera
 
-# Device Tree
-#git clone https://github.com/SenseiiX/android_device_xiaomi_munch device/xiaomi/munch; \
-git clone https://github.com/SenseiiX/android_device_xiaomi_munch -b v-axion device/xiaomi/munch; \
-
-# Munch Vendor
-git clone https://github.com/SenseiiX/android_vendor_xiaomi_munch vendor/xiaomi/munch; \
-
-# Munch Firmware
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
-
-# Kernel
-git clone https://github.com/SenseiiX/fusionX_sm8250 -b exp-nxt kernel/xiaomi/munch; \
-cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
-
-# Hardware Xiaomi
-git clone https://github.com/SenseiiX/android_hardware_xiaomi hardware/xiaomi; \
-
-# Hardware Dolby
-git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
+# ======CLEAN BUILD=======
+#git clone https://github.com/SenseiiX/android_device_xiaomi_munch -b lunaris device/xiaomi/munch
+#git clone https://github.com/munch-devs/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
+#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
+#git clone https://github.com/munch-devs/kernel_xiaomi_munch -b munch-ksu kernel/xiaomi/munch
+#git clone https://github.com/munch-devs/android_hardware_xiaomi hardware/xiaomi
+#git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
+#git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
+#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
 # Device Settings
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
+rm -rf packages/resources/devicesettings
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings --depth 1 -b aosop-16 packages/resources/devicesettings
 
-# Miui Camera
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+# Settings App
+rm -rf packages/apps/Settings
+git clone https://github.com/SenseiiX/packages_apps_Settings --depth 1 -b 16 packages/apps/Settings
 
-
-. build/envsetup.sh; \
-
-brunch munch; \
-#riseup munch eng && rise b; \
-#lunch infinity_munch-user; \
+. build/envsetup.sh
+lunch lineage_munch-bp2a-user
+m lunaris
