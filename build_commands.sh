d23cf62c8066bbc02a6741cbbe8ae357a9429914
SENX | センクス <senseixdev@gmail.com>
2025-08-28 23:21:21 +0000

    1

--- build_commands.sh
+++ build_commands.sh
@@ -2,45 +2,45 @@
 
 rm -rf .repo/local_manifests; \
 
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs; \
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
 
 rm -rf prebuilts/clang/host/linux-x86; \
 
 /opt/crave/resync.sh; \
 
 rm -rf device/xiaomi/munch; \
-#rm -rf kernel/xiaomi/munch; \
-#rm -rf vendor/xiaomi/munch; \
-#rm -rf vendor/xiaomi/munch-firmware; \
-#rm -rf hardware/xiaomi; \
-#rm -rf hardware/dolby; \
-#rm -rf vendor/xiaomi/miuicamera; \
-#rm -rf packages/resources/devicesettings; \
+rm -rf kernel/xiaomi/munch; \
+rm -rf vendor/xiaomi/munch; \
+rm -rf vendor/xiaomi/munch-firmware; \
+rm -rf hardware/xiaomi; \
+rm -rf hardware/dolby; \
+rm -rf vendor/xiaomi/miuicamera; \
+rm -rf packages/resources/devicesettings; \
 
 # Device Tree
-git clone https://github.com/SenseiiX/android_device_xiaomi_munch -b axion device/xiaomi/munch; \
+git clone https://github.com/SenseiiX/android_device_xiaomi_munch -b axion-16 device/xiaomi/munch; \
 
 # Munch Vendor
-#git clone https://github.com/munch-devs/android_vendor_xiaomi_munch vendor/xiaomi/munch; \
+git clone https://github.com/munch-devs/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
 
 # Munch Firmware
-#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
 # Kernel (N0kernel / FusionX)
-#git clone https://github.com/SenseiiX/fusionX_sm8250 -b wip-next kernel/xiaomi/munch; \
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b stable-next kernel/xiaomi/munch; \
 #git clone https://github.com/munch-devs/kernel_xiaomi_munch -b munch-ksu kernel/xiaomi/munch; \
 
 # Hardware Xiaomi
-#git clone https://github.com/SenseiiX/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi; \
+git clone https://github.com/munch-devs/android_hardware_xiaomi.git hardware/xiaomi; \
 
 # Hardware Dolby
-#git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
+git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
 
 # MiCam
-#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
 # Device Settings
-#git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-15 packages/resources/devicesettings; \
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-16 packages/resources/devicesettings; \
 
 . build/envsetup.sh; \
 brunch munch; \
